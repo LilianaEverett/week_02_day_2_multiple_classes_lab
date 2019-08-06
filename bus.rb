363524eec23e543429ac004bbd61e729c633dd6f
Liliana Everett <everettliliana@gmail.com>
2019-08-06 16:26:39 +0100

    part B adding a passenger to the bus

--- bus.rb
+++ bus.rb
@@ -1,16 +1,27 @@
 class Bus
 
-attr_reader(:route_number, :destination)
+attr_reader(:route_number, :destination, :passengers)
 
-  def initialize(route_number, destination)
+  def initialize(route_number, destination, passengers)
     @route_number = route_number
     @destination = destination
-
+    @passengers = passengers
   end
 
 
   def make_driving_sound()
     return "Brum brum"
   end
+
+  def count_passengers()
+      return @passengers.count()
+  end
+
+  def pick_up(passenger)
+    passengers.push(passenger)
+  end
+
+
+
 
 end
